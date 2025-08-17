9d41da499610054efb3bab2a3efad6a4317e36af
JacklineMugo <jackiemugo97@gmail.com>
2025-08-18 00:15:45 +0300

    Update calculate_discount.py

--- calculate_discount.py
+++ calculate_discount.py
@@ -1,11 +1,14 @@
+# Discount Function 
 def calculate_discount(price, discount_percent):
     if discount_percent >= 20:
         return price - (price * discount_percent / 100)
     else:
         return price
 
+# Takes users input 
 price = float(input("Enter the original price: "))
 discount_percent = float(input("Enter the discount percentage: "))
 
 final_price = calculate_discount(price, discount_percent)
 print("Final price:", final_price)
+
